f61555fd794b59f034845cc1b83f8c5678fa15cf
Louis <louis.m.newton@hotmail.fr>
2021-09-09 12:19:36 +0200

    récupération des Pin de Mysql avec doctrine et insertion des pins dans les pages

--- src/Controller/PinsController.php
+++ src/Controller/PinsController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Pin;
+use App\Repository\PinRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PinsController extends AbstractController
 {
     #[Route('/pins', name: 'app_home')]
-    public function index(): Response
+    public function index(PinRepository $repo): Response
     {
+        // $repo = $this->getDoctrine()->getRepository(Pin::class);
+        $pins = $repo->findAll();
+
         return $this->render('pins/index.html.twig', [
             'controller_name' => 'PinsController',
+            'pins' => $pins,
         ]);
     }
 
@@ -25,12 +31,11 @@ class PinsController extends AbstractController
         ]);
     }
 
-    #[Route('/pins/12', name: 'pin_show')]
-    public function show(): Response
+    #[Route('/pins/{id}', name: 'pin_show')]
+    public function show(Pin $pin): Response
     {
         return $this->render('pins/show.html.twig', [
-            'title' => 'Louis',
-            'age' => 31
+            'pin' => $pin
         ]);
     }
 }
